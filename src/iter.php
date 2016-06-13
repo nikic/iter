8b16bcc2850bff80c1824c4100660d03e659c152
Josh Di Fabio <joshdifabio@gmail.com>
2016-06-13 17:07:11 +0100

    Micro-optimize if,if to if,elseif

--- src/iter.php
+++ src/iter.php
@@ -48,8 +48,7 @@ function range($start, $end, $step = null) {
     } else {
         if (null === $step) {
             $step = -1;
-        }
-        if ($step >= 0) {
+        } elseif ($step >= 0) {
             throw new \InvalidArgumentException(
                 'If start > end the step must be negative'
             );
