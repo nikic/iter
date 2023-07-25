d9f88bc04b5b453914373e70c041353d8e67c3f5
Andrew Moyes <moyes.andrew@gmail.com>
2023-07-25 21:55:40 +0200

    Make phpdoc types more precise (#93)
    Improve type annotations by making use of templates, callable signatures
    and other type system features supported by psalm and phpstan.
    
    Additionally, this makes the following changes:
     * Fix CI to work on PHP 7.1 again.
     * Add psalm and phpstan to require-dev for validation.
     * Handle IteratorAggregate returning a non-Iterator in isEmpty().
     * In functions that take a `$levels = INF` parameter or similar, changed
      `INF` to `PHP_INT_MAX` as `INF` is a float type and not int as declared.
      In these cases, `$levels` can never exceed the `PHP_INT_MAX` anyhow.
    * Split off RewindableGenerator from RewindableIterator.
    * Psalm does not have support for recursive types. Functions which use
     recursive types have had a note added to the docblock to explain this.
    * I've not added docblocks to the functions in the `\iter\rewindable`
      namespace, as that would necessarily mean that any changes to the
      regular function would also need to be reflected in the docblocks for
      these methods. This does mean that these functions do not benefit from
      the Psalm type annotations currently.
    
    Co-authored-by: Andrew Moyes <andrew.moyes@futurenet.com>

--- src/iter.rewindable.php
+++ src/iter.rewindable.php
@@ -2,10 +2,10 @@
 
 namespace iter {
     /**
-     * Converts a generator function into a rewindable generator function.
+     * Creates a rewindable iterator from a non-rewindable iterator.
      *
      * This is implemented simply be remembering the arguments with which the
-     * generator function is later called and just calling it again if a
+     * factory function is later called and just calling it again if a
      * rewind() occurs.
      *
      * Example:
@@ -14,36 +14,42 @@ namespace iter {
      *      $res = $rewindableMap(func\operator('*', 3), [1, 2, 3]);
      *      // $res is a rewindable iterator with elements [3, 6, 9]
      *
-     * @param callable $function Generator function to make rewindable
+     * @template TKey
+     * @template TValue
      *
-     * @return callable Rewindable generator function
+     * @param callable(...mixed):\Iterator<TKey,TValue> $function Iterator factory function
+     *
+     * @return callable(...mixed):\Iterator<TKey,TValue> Rewindable iterator factory function
      */
     function makeRewindable(callable $function) {
         return function(...$args) use ($function) {
-            return new rewindable\_RewindableGenerator($function, $args);
+            return new rewindable\_RewindableIterator($function, $args);
         };
     }
 
     /**
-     * Call a generator function, but make the result rewindable.
+     * Creates a rewindable iterator from a non-rewindable iterator.
      *
-     * This function does basically the same thing as makeRewindable(), but it
-     * directly calls the function, rather than returning a lambda. Useful if
-     * you want to do a one-off call, rather than using the rewindable function
-     * multiple times.
+     * This function does basically the same thing as makeRewindable(),
+     * but it directly calls the function, rather than returning a lambda.
+     * Useful if you want to do a one-off call, rather than using the rewindable
+     * function multiple times.
      *
      * Example:
      *
      *      $res = iter\callRewindable('iter\\map', func\operator('*', 3), [1, 2, 3]);
      *      // $res is a rewindable iterator with elements [3, 6, 9]
      *
-     * @param callable $function Generator function to call rewindably
+     * @template TKey
+     * @template TValue
+     *
+     * @param callable(...mixed):\Iterator<TKey,TValue> $function Iterator factory function
      * @param mixed ...$args Function arguments
      *
-     * @return \Iterator Rewindable generator result
+     * @return \Iterator<TKey,TValue> Rewindable generator result
      */
     function callRewindable(callable $function, ...$args) {
-        return new rewindable\_RewindableGenerator($function, $args);
+        return new rewindable\_RewindableIterator($function, $args);
     }
 }
 
@@ -56,66 +62,79 @@ namespace iter\rewindable {
      * non-rewindable functions from the iter namespace
      */
 
-    function range()         { return new _RewindableGenerator('iter\range',         func_get_args()); }
-    function map()           { return new _RewindableGenerator('iter\map',           func_get_args()); }
-    function mapKeys()       { return new _RewindableGenerator('iter\mapKeys',       func_get_args()); }
-    function mapWithKeys()   { return new _RewindableGenerator('iter\mapWithKeys',   func_get_args()); }
-    function flatMap()       { return new _RewindableGenerator('iter\flatMap',       func_get_args()); }
-    function reindex()       { return new _RewindableGenerator('iter\reindex',       func_get_args()); }
-    function filter()        { return new _RewindableGenerator('iter\filter',        func_get_args()); }
-    function enumerate()     { return new _RewindableGenerator('iter\enumerate',     func_get_args()); }
-    function toPairs()       { return new _RewindableGenerator('iter\toPairs',       func_get_args()); }
-    function fromPairs()     { return new _RewindableGenerator('iter\fromPairs',     func_get_args()); }
-    function reductions()    { return new _RewindableGenerator('iter\reductions',    func_get_args()); }
-    function zip()           { return new _RewindableGenerator('iter\zip',           func_get_args()); }
-    function zipKeyValue()   { return new _RewindableGenerator('iter\zipKeyValue',   func_get_args()); }
-    function chain()         { return new _RewindableGenerator('iter\chain',         func_get_args()); }
-    function product()       { return new _RewindableGenerator('iter\product',       func_get_args()); }
-    function slice()         { return new _RewindableGenerator('iter\slice',         func_get_args()); }
-    function take()          { return new _RewindableGenerator('iter\take',          func_get_args()); }
-    function drop()          { return new _RewindableGenerator('iter\drop',          func_get_args()); }
-    function repeat()        { return new _RewindableGenerator('iter\repeat',        func_get_args()); }
-    function takeWhile()     { return new _RewindableGenerator('iter\takeWhile',     func_get_args()); }
-    function dropWhile()     { return new _RewindableGenerator('iter\dropWhile',     func_get_args()); }
-    function keys()          { return new _RewindableGenerator('iter\keys',          func_get_args()); }
-    function values()        { return new _RewindableGenerator('iter\values',        func_get_args()); }
-    function flatten()       { return new _RewindableGenerator('iter\flatten',       func_get_args()); }
-    function flip()          { return new _RewindableGenerator('iter\flip',          func_get_args()); }
-    function chunk()         { return new _RewindableGenerator('iter\chunk',         func_get_args()); }
-    function chunkWithKeys() { return new _RewindableGenerator('iter\chunkWithKeys', func_get_args()); }
+    function range()         { return new _RewindableIterator('iter\range',         func_get_args()); }
+    function map()           { return new _RewindableIterator('iter\map',           func_get_args()); }
+    function mapKeys()       { return new _RewindableIterator('iter\mapKeys',       func_get_args()); }
+    function mapWithKeys()   { return new _RewindableIterator('iter\mapWithKeys',   func_get_args()); }
+    function flatMap()       { return new _RewindableIterator('iter\flatMap',       func_get_args()); }
+    function reindex()       { return new _RewindableIterator('iter\reindex',       func_get_args()); }
+    function filter()        { return new _RewindableIterator('iter\filter',        func_get_args()); }
+    function enumerate()     { return new _RewindableIterator('iter\enumerate',     func_get_args()); }
+    function toPairs()       { return new _RewindableIterator('iter\toPairs',       func_get_args()); }
+    function fromPairs()     { return new _RewindableIterator('iter\fromPairs',     func_get_args()); }
+    function reductions()    { return new _RewindableIterator('iter\reductions',    func_get_args()); }
+    function zip()           { return new _RewindableIterator('iter\zip',           func_get_args()); }
+    function zipKeyValue()   { return new _RewindableIterator('iter\zipKeyValue',   func_get_args()); }
+    function chain()         { return new _RewindableIterator('iter\chain',         func_get_args()); }
+    function product()       { return new _RewindableIterator('iter\product',       func_get_args()); }
+    function slice()         { return new _RewindableIterator('iter\slice',         func_get_args()); }
+    function take()          { return new _RewindableIterator('iter\take',          func_get_args()); }
+    function drop()          { return new _RewindableIterator('iter\drop',          func_get_args()); }
+    function repeat()        { return new _RewindableIterator('iter\repeat',        func_get_args()); }
+    function takeWhile()     { return new _RewindableIterator('iter\takeWhile',     func_get_args()); }
+    function dropWhile()     { return new _RewindableIterator('iter\dropWhile',     func_get_args()); }
+    function keys()          { return new _RewindableIterator('iter\keys',          func_get_args()); }
+    function values()        { return new _RewindableIterator('iter\values',        func_get_args()); }
+    function flatten()       { return new _RewindableIterator('iter\flatten',       func_get_args()); }
+    function flip()          { return new _RewindableIterator('iter\flip',          func_get_args()); }
+    function chunk()         { return new _RewindableIterator('iter\chunk',         func_get_args()); }
+    function chunkWithKeys() { return new _RewindableIterator('iter\chunkWithKeys', func_get_args()); }
 
     /**
-     * This class is used for the internal implementation of rewindable
-     * generators. Should not be used directly, instead use makeRewindable() or
-     * callRewindable().
+     * This class is used for the internal implementation of iterators that are
+     * otherwise not rewindable. Should not be used directly, instead use
+     * makeRewindable() or callRewindable().
+     *
+     * @template TKey
+     * @template TValue
+     *
+     * @implements \Iterator<TKey,TValue>
      *
      * @internal
      */
-    class _RewindableGenerator implements \Iterator {
+    class _RewindableIterator implements \Iterator {
+        /** @var callable */
         protected $function;
-        protected $args;
-        /** @var \Generator */
-        protected $generator;
 
+        /** @var mixed[] */
+        protected $args;
+
+        /** @var \Iterator<TKey,TValue> */
+        protected $iterator;
+
+        /**
+         * @param callable(...mixed):\Iterator<TKey,TValue> $function
+         * @param mixed[] $args
+         */
         public function __construct(callable $function, array $args) {
             $this->function = $function;
             $this->args = $args;
-            $this->generator = null;
+            $this->iterator = null;
         }
 
         public function rewind(): void {
             $function = $this->function;
-            $this->generator = $function(...$this->args);
+            $this->iterator = $function(...$this->args);
         }
 
         public function next(): void {
-            if (!$this->generator) { $this->rewind(); }
-            $this->generator->next();
+            if (!$this->iterator) { $this->rewind(); }
+            $this->iterator->next();
         }
 
         public function valid(): bool {
-            if (!$this->generator) { $this->rewind(); }
-            return $this->generator->valid();
+            if (!$this->iterator) { $this->rewind(); }
+            return $this->iterator->valid();
         }
 
         /**
@@ -123,8 +142,8 @@ namespace iter\rewindable {
          */
         #[ReturnTypeWillChange]
         public function key() {
-            if (!$this->generator) { $this->rewind(); }
-            return $this->generator->key();
+            if (!$this->iterator) { $this->rewind(); }
+            return $this->iterator->key();
         }
 
         /**
@@ -132,18 +151,56 @@ namespace iter\rewindable {
          */
         #[ReturnTypeWillChange]
         public function current() {
-            if (!$this->generator) { $this->rewind(); }
-            return $this->generator->current();
+            if (!$this->iterator) { $this->rewind(); }
+            return $this->iterator->current();
+        }
+    }
+
+    /**
+     * This class was used for the internal implementation of rewindable
+     * generators. This has been deprecated in favor of the more general
+     * _RewindableIterator class, and may be removed in a future version.
+     *
+     * @template TKey
+     * @template TYield
+     * @template TSend
+     * @template TReturn
+     *
+     * @extends _RewindableIterator<TKey,TYield>
+     *
+     * @internal
+     * @deprecated
+     */
+    class _RewindableGenerator extends _RewindableIterator {
+        /** @var \Generator<TKey,TYield,TSend,TReturn> */
+        protected $iterator;
+
+        /**
+         * @param callable(...mixed):\Generator<TKey,TYield,TSend,TReturn> $function
+         * @param mixed[] $args
+         */
+        public function __construct(callable $function, array $args) {
+            $this->function = $function;
+            $this->args = $args;
+            $this->iterator = null;
         }
 
+        /**
+         * @param mixed $value
+         * @return TYield|null
+         */
         public function send($value = null) {
-            if (!$this->generator) { $this->rewind(); }
-            return $this->generator->send($value);
+            if (!$this->iterator) { $this->rewind(); }
+            return $this->iterator->send($value);
         }
 
+        /**
+         * @param \Throwable $exception
+         * @return TYield
+         */
         public function throw($exception) {
-            if (!$this->generator) { $this->rewind(); }
-            return $this->generator->throw($exception);
+            if (!$this->iterator) { $this->rewind(); }
+            return $this->iterator->throw($exception);
         }
     }
 }
